134229fb15e0b8292015dc69ff8e1570e8b34b36
Шолохов Даниил <sholokhovdaniil@yandex.ru>
2025-12-31 17:49:52 +0700

    work(event): Создан менеджер событий

---
+++ src/Event/EventManager.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Sholokhov\Sitemap\Events;
+
+use Exception;
+
+use Bitrix\Main\Event;
+use Bitrix\Main\EventManager as BXEventManager;
+use Bitrix\Main\ArgumentTypeException;
+
+/**
+ * Менеджер событий модуля
+ *
+ * Является оберткой над {@see BXEventManager}
+ */
+class EventManager
+{
+    protected string $moduleID;
+    protected static self $instance;
+
+    /**
+     * Зарегистрированные события.
+     *
+     * @var array[]
+     * @author Daniil S. GlobalArts
+     */
+    protected array $events = [];
+
+    protected function __construct()
+    {
+        $this->moduleID = 'sholokhov.sitemap';
+    }
+
+    /**
+     * Вызов события.
+     *
+     * @param string $id
+     * @param array $parameters
+     * @return void
+     * @throws ArgumentTypeException
+     * @throws Exception
+     */
+    public function call(string $id, array &$parameters = []): void
+    {
+        $data = $this->get($id);
+        $data['event']->setParameters($parameters);
+        $data['event']->send();
+
+        if (null === $data['callback']) {
+            return;
+        }
+
+        foreach ($data['event']->getResults() as $eventResult) {
+            call_user_func($data['callback'], $eventResult);
+        }
+    }
+
+    /**
+     * Подписка на событие.
+     *
+     * @param string $id
+     * @param callable $callback
+     * @param int $sort
+     * @return void
+     */
+    public function subscribe(string $id, callable $callback, int $sort = 500): void
+    {
+        BXEventManager::getInstance()->addEventHandler(
+            $this->moduleID,
+            $id,
+            $callback,
+            false,
+            $sort
+        );
+    }
+
+    /**
+     * Регистрация события.
+     *
+     * @param string $id Тип события
+     * @param callable|null $callback Обработчик результатов события
+     * @return self
+     */
+    public function registration(string $id, callable $callback = null): self
+    {
+        $this->events[$id] = [
+            'event' => new Event($this->moduleID, $id),
+            'callback' => $callback
+        ];
+
+        return $this;
+    }
+
+    /**
+     * Вызов события.
+     *
+     * @param string $id
+     * @return array{event: Event, callback: callable|null}
+     * @throws Exception
+     */
+    public function get(string $id): array
+    {
+        $this->check($id);
+        return $this->events[$id];
+    }
+
+    /**
+     * Проверка наличия события.
+     *
+     * @param string $id
+     * @return bool
+     */
+    public function has(string $id): bool
+    {
+        return array_key_exists($id, $this->events);
+    }
+
+    /**
+     * Проверка наличия события.
+     *
+     * @param string $id
+     * @return void
+     * @throws Exception
+     */
+    protected function check(string $id): void
+    {
+        if (!$this->has($id)) {
+            throw new Exception('Event not registered');
+        }
+    }
+
+    /**
+     * Получение менеджера события.
+     *
+     * @return self
+     */
+    public static function getInstance(): self
+    {
+        return static::$instance ??= new static;
+    }
+}
